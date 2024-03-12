e3b539da8ed8d8a74fae3b0b356e6f2974ec7efb
keviny007 <kevin@yangfamily.com>
2024-03-11 20:26:32 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 # Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
 # and rename this file to constants.py.
-APIKEY = "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"
+APIKEY = "<open ai key>"
